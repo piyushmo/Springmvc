bd96eeb5542a4c5cacb00a9a846c0126b2ea0c34
piyushmo <piyushmore.888@gmail.com>
2023-04-05 23:07:00 +0530

    Spring-MVC

---
+++ Spring-MVC/src/main/java/com/example/spring/mvc/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.example.spring.mvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class WelcomeController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		// TODO Auto-generated method stub
+		return new ModelAndView("welcome", "message", "Welcome to Spring MVC App");
+	}
+	
+	
+
+}
